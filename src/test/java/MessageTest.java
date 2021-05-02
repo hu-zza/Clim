74c01c3a7c90ea2e3e3e285cace4201a25cc712a
Szabó László András <noreply@zza.hu>
2021-05-02 23:22:38 +0200

    Add ResourceBundle for Message class

---
+++ src/test/java/MessageTest.java
@@ -0,0 +1,33 @@
+/*
+ *
+ * clim  //  Command Line Interface Menu
+ *       //  https://git.zza.hu/clim
+ *
+ * Copyright (C) 2020-2021 Szabó László András // hu-zza
+ *
+ * This file is part of clim.
+ *
+ * clim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * clim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import hu.zza.clim.Message;
+
+public class MessageTest {
+  public static void main(String[] args) {
+    Object[] nul = {"", "", "", "", "", "", "", ""};
+    for (var e : Message.values()) {
+      System.out.println(e.getMessage(nul) + "\n\n" + "#".repeat(20) + "\n");
+    }
+  }
+}
