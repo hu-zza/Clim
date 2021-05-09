283a3f5ad6cb9b093a9c77bff9174525aa82e317
Szabó László András <noreply@zza.hu>
2021-05-09 19:32:11 +0200

    Cleaner interface for the simple building, but still needs business logic.

--- src/main/java/hu/zza/clim/MenuBuilder.java
+++ src/main/java/hu/zza/clim/MenuBuilder.java
@@ -30,53 +30,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MenuBuilder {
+  private ControlType controlType = ControlType.ORDINAL;
+  private String initialPosition = "root";
+  private String commandRegex = "\\s*(\\w+)\\s*";
   private JSONObject menuStructure;
-  private JSONObject hiddenStructure;
   private Class<? extends ParameterName> parameterNameEnum;
-  private ControlType controlType;
-  private String initialPosition;
-  private String commandRegex;
   private final Map<String, Leaf> leafMap = new HashMap<>();
-  private final Map<String, String> delimiterMap = new HashMap<>();
   private final Map<String, List<ParameterName>> parameterNameMap = new HashMap<>();
   private final Map<String, List<Parameter>> parameterMap = new HashMap<>();
-
+  private final Map<String, String> delimiterMap = new HashMap<>();
 
   public Menu build() {
-    System.out.println("" + leafMap + delimiterMap + parameterNameMap + parameterMap);
     // return Menu.of(menuStructure, controlType, parameterMatcher, initialPosition);
     return null;
   }
 
   public MenuBuilder clear() {
-    menuStructure = null;
-    hiddenStructure = null;
-    parameterNameEnum = null;
     controlType = null;
     initialPosition = null;
-    commandRegex = null;
+    commandRegex = "\\s*(\\w+)\\s*";
+    menuStructure = null;
+    parameterNameEnum = null;
     leafMap.clear();
-    delimiterMap.clear();
     parameterNameMap.clear();
     parameterMap.clear();
-    return this;
-  }
-
-  public MenuBuilder setMenuStructure(JSONObject menuStructure) {
-    this.menuStructure = menuStructure;
-    return this;
-  }
-
-  public MenuBuilder setHiddenStructure(JSONObject hiddenStructure) {
-    this.hiddenStructure = hiddenStructure;
-    return this;
-  }
-
-  public MenuBuilder setParameterNameEnum(Class<? extends ParameterName> parameterNameEnum) {
-    this.parameterNameEnum = parameterNameEnum;
+    delimiterMap.clear();
     return this;
   }
 
@@ -95,6 +77,21 @@ public class MenuBuilder {
     return this;
   }
 
+  public MenuBuilder setMenuStructure(String menuStructureJSON) throws JSONException {
+    this.menuStructure = new JSONObject(menuStructureJSON);
+    return this;
+  }
+
+  public MenuBuilder setMenuStructure(JSONObject menuStructure) {
+    this.menuStructure = menuStructure;
+    return this;
+  }
+
+  public MenuBuilder setParameterNameEnum(Class<? extends ParameterName> parameterNameEnum) {
+    this.parameterNameEnum = parameterNameEnum;
+    return this;
+  }
+
   public MenuBuilder setLeaf(
       String name, Function<ProcessedInput, Integer> function, String... links) {
     leafMap.put(name, new Leaf(name, function, links));
@@ -102,10 +99,13 @@ public class MenuBuilder {
   }
 
   public MenuBuilder setLeafParameters(
-      String leafName, String delimiter, List<ParameterName> parameterNames, List<Parameter> parameters) {
-    delimiterMap.put(leafName, delimiter);
+      String leafName,
+      String delimiter,
+      List<ParameterName> parameterNames,
+      List<Parameter> parameters) {
     parameterNameMap.put(leafName, parameterNames);
     parameterMap.put(leafName, parameters);
+    delimiterMap.put(leafName, delimiter);
     return this;
   }
 
---
+++ src/test/java/MenuBuilderTest.java
@@ -0,0 +1,58 @@
+/*
+ *
+ * clim  //  Command Line Interface Menu
+ *       //  https://git.zza.hu/clim
+ *
+ * Copyright (C) 2020-2021 Szabó László András // hu-zza
+ *
+ * This file is part of clim.
+ *
+ * clim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * clim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import hu.zza.clim.ControlType;
+import hu.zza.clim.Menu;
+import hu.zza.clim.MenuBuilder;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MenuBuilderTest {
+  public static void main(String[] args) throws IOException {
+
+    Path structurePath = Path.of("src", "test", "resources", "MenuStructure.txt");
+
+    String menuStructure =
+        String.join(
+            "", Files.readAllLines(structurePath));
+
+    Menu menu =
+        new MenuBuilder()
+            .setControlType(ControlType.ORDINAL)
+            .setMenuStructure(menuStructure)
+            .setLeaf("leaf1", a -> 0, "node2", "node3", "node1")  // node2  pibling
+            .setLeaf("leaf2", a -> 1, "node3", "node4", "node1")  // node4  hidden top
+            .setLeaf("leaf3", a -> 2, "node2", "node3", "node1")  // node1  pibling
+            .setLeaf("leaf4", a -> 0, "root", "node1", "node2")   // root   great-grandparent
+            .setLeaf("leaf5", a -> 1, "node2", "node3", "node1")  // node3  sibling
+            .setLeaf("leaf6", a -> 2, "node4", "node7", "node9")  // node9  hidden bottom
+            .setLeaf("leaf7", a -> 0, "root", "node3", "node1")   // root   parent
+            .setLeaf("leaf8", a -> 1, "node2", "root", "node1")   // root   great-grandparent
+            .setLeaf("leaf9", a -> 2, "root", "node3", "node10")  // node10 first-cousin once removed
+            .setLeaf("leaf10", a -> 0, "node5", "root", "node1")  // node5  pibling
+            .setLeaf("leaf11", a -> 1, "node2", "node3", "root")  // node3  visible bottom
+            .setLeaf("leaf12", a -> 2, "node2", "root", "node5")  // node5  pibling
+            .build();
+  }
+}
