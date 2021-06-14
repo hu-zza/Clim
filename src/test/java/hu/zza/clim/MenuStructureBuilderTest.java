98e51686c0a56b9491f1ca8254c48cb151019f0c
Szabó László András <noreply@zza.hu>
2021-06-14 20:17:14 +0200

    Using @RepeatedTest

--- src/test/java/hu/zza/clim/MenuStructureBuilderTest.java
+++ src/test/java/hu/zza/clim/MenuStructureBuilderTest.java
@@ -21,21 +21,27 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package hu.zza.clim;import java.io.IOException;
+package hu.zza.clim;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 
 public class MenuStructureBuilderTest {
-  private static final Path structurePath = Path.of("src", "test", "resources", "MenuStructure.txt");
-  private static final MenuStructureBuilder builder = new MenuStructureBuilder();
+  private static final Path structurePath =
+      Path.of("src", "test", "resources", "MenuStructure.txt");
+  private static MenuStructureBuilder builder = new MenuStructureBuilder();
   private static String rawMenuStructure;
 
   @BeforeEach
   void resetAll() {
-    builder.clear();
+    builder = new MenuStructureBuilder();
     resetRawMenuStructureString();
   }
 
@@ -47,23 +53,24 @@ public class MenuStructureBuilderTest {
     }
   }
 
-  @Test
-  void testMissingSettings() {
-    for (int i = 0; i < 7; i++) {
-      builder.clear();
-      if (i % 2 == 1) {
-        builder.setRawMenuStructure(rawMenuStructure);
-      }
 
-      if ((i>>1) % 2 == 1) {
-        builder.setInitialPosition("node2");
-      }
+  @RepeatedTest(7)
+  @DisplayName("[batch] Build omitting (some) parts completely")
+  void testMissingSettings(RepetitionInfo repetitionInfo) {
+    int i = repetitionInfo.getCurrentRepetition() - 1; // range [0 - 6]
 
-      if ((i>>2) % 2 == 1) {
-        setLeaves();
-      }
-      Assertions.assertThrows(ClimException.class, builder::build);
+    if (i % 2 == 1) {
+      builder.setRawMenuStructure(rawMenuStructure);
     }
+
+    if ((i >> 1) % 2 == 1) {
+      builder.setInitialPosition("node2");
+    }
+
+    if ((i >> 2) % 2 == 1) {
+      setLeaves();
+    }
+    Assertions.assertThrows(ClimException.class, builder::build);
   }
 
   private void setLeaves() {
@@ -107,7 +114,7 @@ public class MenuStructureBuilderTest {
         .setLeaf("leaf5", a -> 1, "node2", "node3", "node1")
         .setLeaf("leaf6", a -> 2, "node4", "node7", "node9")
         .setLeaf("leaf7", a -> 0, "root", "node3", "node1")
-        //.setLeaf("leaf8", a -> 1, "node2", "root", "node1")
+        // .setLeaf("leaf8", a -> 1, "node2", "root", "node1")
         .setLeaf("leaf9", a -> 2, "root", "node3", "node10")
         .setLeaf("leaf10", a -> 0, "node5")
         .setLeaf("leaf11", a -> 1, "node2", "node3", "root")
